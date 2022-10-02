8e21d323b514c4880a1034c6f29032a2b7151c29
Rockwell Schrock <rockwell@schrock.me>
2022-10-02 14:25:49 -0400

    Rename injected style classes; add pid

--- lib/live_inspect/theme/light.ex
+++ lib/live_inspect/theme/light.ex
@@ -4,14 +4,14 @@ defmodule LiveInspect.Theme.Light do
   @impl true
   def style do
     """
-    .live-dump-table {
-      background: #ffffff;
-      font-color: #000000;
+    .live-inspect-table {
+      background: #fff;
+      font-color: #000;
       font-size: 0.8rem;
     }
 
-    .live-dump-table td,
-    .live-dump-table th {
+    .live-inspect-table td,
+    .live-inspect-table th {
       vertical-align: top;
       border: 1px solid #999;
       border-collapse: collapse;
@@ -19,34 +19,35 @@ defmodule LiveInspect.Theme.Light do
       text-align: left;
     }
 
-    .live-dump-table th {
+    .live-inspect-table th {
       cursor: pointer;
       font-weight: bold;
-      background-color: #dddddd;
+      background-color: #ddd;
       user-select: none;
       -moz-user-select: none;
       -webkit-user-select: none;
       -ms-user-select: none;
     }
 
-    .live-dump-table th:hover {
-      background-color: #bbbbbb;
+    .live-inspect-table th:hover {
+      background-color: #bbb;
     }
 
-    .live-dump-number { color: #e00; }
-    .live-dump-binary { color: #00e; }
-    .live-dump-struct { color: #0aa; }
-    .live-dump-map { color: #0a0; }
-    .live-dump-list { color: #a0a; }
-    .live-dump-atom { color: #000; }
-    .live-dump-tuple { color: #aa0; }
-    .live-dump-other { color: #000; }
+    .live-inspect-number { color: #e00; }
+    .live-inspect-binary { color: #00e; }
+    .live-inspect-struct { color: #0aa; }
+    .live-inspect-map { color: #0a0; }
+    .live-inspect-list { color: #a0a; }
+    .live-inspect-atom { color: #000; }
+    .live-inspect-tuple { color: #aa0; }
+    .live-inspect-pid { color: #000; }
+    .live-inspect-other { color: #000; }
     """
   end
 
   @impl true
   def table_attrs do
-    %{class: "live-dump-table"}
+    %{class: "live-inspect-table"}
   end
 
   @impl true
@@ -59,12 +60,13 @@ defmodule LiveInspect.Theme.Light do
     %{class: value_class(value)}
   end
 
-  defp value_class(number) when is_number(number), do: "live-dump-number"
-  defp value_class(binary) when is_binary(binary), do: "live-dump-binary"
-  defp value_class(struct) when is_struct(struct), do: "live-dump-struct"
-  defp value_class(map) when is_map(map), do: "live-dump-map"
-  defp value_class(list) when is_list(list), do: "live-dump-list"
-  defp value_class(atom) when is_atom(atom), do: "live-dump-atom"
-  defp value_class(tuple) when is_tuple(tuple), do: "live-dump-tuple"
-  defp value_class(_else), do: "live-dump-other"
+  defp value_class(number) when is_number(number), do: "live-inspect-number"
+  defp value_class(binary) when is_binary(binary), do: "live-inspect-binary"
+  defp value_class(struct) when is_struct(struct), do: "live-inspect-struct"
+  defp value_class(map) when is_map(map), do: "live-inspect-map"
+  defp value_class(list) when is_list(list), do: "live-inspect-list"
+  defp value_class(atom) when is_atom(atom), do: "live-inspect-atom"
+  defp value_class(tuple) when is_tuple(tuple), do: "live-inspect-tuple"
+  defp value_class(pid) when is_pid(pid), do: "live-inspect-pid"
+  defp value_class(_else), do: "live-inspect-other"
 end
